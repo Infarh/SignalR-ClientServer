7c758fb6479d42c8f5f5c32e4b5727fed8f3cd4d
Infarh <shmachilin@yandex.ru>
2020-07-26 00:28:53 +0300

    Взаимодействие

--- SignalR-Client/Program.cs
+++ SignalR-Client/Program.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace SignalR_Client
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
-        {
+        private static readonly CancellationTokenSource __ReadLineCancellation = new CancellationTokenSource();
 
+        private static async Task Main(string[] args)
+        {
+            Console.CancelKeyPress += OnConsole_CancelKeyPressed;
+
+            const string address = "http://localhost:5000/information";
+            var connection = new HubConnectionBuilder()
+               .WithUrl(address)
+               .Build();
+            connection.On("ClientConnected", OnClientConnected);
+            connection.On("ClientDisconnected", OnClientDisconnected);
+            connection.On<DateTime, string>("IntroduceNewClient", OnIntroduceNewClient);
+            connection.On<DateTime, string, string>("ClientMessage", ReceiveMessage);
+
+            Console.Write("User name >");
+            var user_name = Console.ReadLine();
+            Console.WriteLine();
+
+            await connection.StartAsync();
+            await connection.SendAsync("IntroduceClient", user_name);
+
+            while (!__ReadLineCancellation.IsCancellationRequested)
+            {
+                var message = await Task.Run(Console.ReadLine).WithCancellation(__ReadLineCancellation.Token);
+                await connection.InvokeAsync("ServerMessage", user_name, message);
+            }
+
+            await connection.StopAsync();
+        }
+
+        private static void OnIntroduceNewClient(DateTime Time, string UserName)
+        {
+            Console.WriteLine("Новый клиент представился как: {0} [{1:dd.MM.yy HH:mm:ss}]", UserName, Time);
+        }
+
+        private static void OnClientConnected()
+        {
+            Console.WriteLine("Новый клиент");
+        }
+
+        private static void OnClientDisconnected()
+        {
+            Console.WriteLine("Клиент отключился");
+        }
+
+        private static void OnConsole_CancelKeyPressed(object Sender, ConsoleCancelEventArgs E)
+        {
+            __ReadLineCancellation.Cancel();
+            E.Cancel = true;
+        }
+
+        private static void ReceiveMessage(DateTime Time, string User, string Message)
+        {
+            Console.WriteLine("{0:dd:MM:yy HH:mm:ss}[{1}]:{2}", Time, User, Message);
+        }
+    }
+
+    internal class UserMessage
+    {
+        public DateTime Time { get; set; }
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+
+    internal static class TaskEx
+    {
+        public static async Task<T> WithCancellation<T>([NotNull] this Task<T> task, CancellationToken cancel)
+        {
+            var source = new TaskCompletionSource<object>();
+            await using (cancel.Register(tcs => ((TaskCompletionSource<object>)tcs).TrySetResult(default), source))
+                if (task != await Task.WhenAny(task, source.Task))
+                    throw new TaskCanceledException();
+            return await task;
         }
     }
 }
---
+++ SignalR-Server/Hubs/InformationHub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace SignalR_Server.Hubs
+{
+    public class InformationHub : Hub
+    {
+        private readonly ILogger<InformationHub> _Logger;
+
+        public InformationHub(ILogger<InformationHub> Logger) => _Logger = Logger;
+
+        public async Task ServerMessage(string User, string Message)
+        {
+            var current_time = DateTime.Now;
+            _Logger.LogInformation("Message from {0} at {1:dd.MM.yy HH:mm:ss}: {2}", User, current_time, Message);
+            await Clients.All.SendAsync("ClientMessage", current_time, User, Message);
+        }
+
+        public async Task IntroduceClient(string UserName)
+        {
+            var current_time = DateTime.Now;
+            _Logger.LogInformation("Новый клиент с именем: {0}[{1:dd.MM.yy HH:mm:ss}]", UserName, current_time);
+            await Clients.All.SendAsync("IntroduceNewClient", current_time, UserName);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            _Logger.LogInformation("Client connected");
+            await Clients.All.SendAsync("ClientConnected");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+            _Logger.LogInformation("Client disconnected");
+            await Clients.All.SendAsync("ClientDisconnected");
+        }
+    }
+}
